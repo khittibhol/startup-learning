b0e8d3041d9da14de06e54a4b8336883b2b11de7
khittibhol <khittibhol@gmail.com>
2020-02-02 23:10:48 +0700

    Answer write here

---
+++ workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
@@ -0,0 +1,34 @@
+package ac.th.ksu.dit;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author khittibhol
+ *
+ */
+public class CalculateExpensiveMobileNumber {
+	
+	public static void main(String[] args) throws IOException {
+
+		FileReader file = new FileReader("resources/number.txt");
+		BufferedReader reader = new BufferedReader(file);
+		try {			
+
+//			To do write code here.
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {			
+			reader.close();
+			file.close();
+		}
+		
+		System.exit(0);
+	}
+
+}
