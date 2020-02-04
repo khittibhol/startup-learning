f664a11e8d20e7e2f025a79f61b1d144bee232a9
N@patwarun <napatwarun.sr@ksu.ac.th>
2020-02-05 01:18:54 +0700

    Update Cal

--- workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
+++ workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
@@ -9,22 +9,33 @@ import java.io.IOException;
  *
  */
 public class CalculateExpensiveMobileNumber {
-	
+
 	public static void main(String[] args) throws IOException {
 
 		FileReader file = new FileReader("resources/number.txt");
 		BufferedReader reader = new BufferedReader(file);
-		try {			
+		try {
+			// To do write code here.
+			int i = 1;
+			String data;
 
-//			To do write code here.
-			
+			// System.out.println(b = bin.readLine()); result is String in b
+
+			while ((data = reader.readLine()) != null) {
+
+				System.out.println(i + " : " + data);
+
+				i = i + 1;
+
+			}
+
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {			
+		} finally {
 			reader.close();
 			file.close();
 		}
-		
+
 		System.exit(0);
 	}
 
--- workshop-java/src/student/Nareerat.java
+++ workshop-java/src/student/Nareerat.java
@@ -1,55 +1,56 @@
 package student;
-import java.util.Scanner;
-public class Nareerat {
-	 public static void main(String[] args) {
-		  // TODO Auto-generated method stub
-		  String name = "nareerat";
-		  String pass = "phukan";
-		  double b = 5000;
-		  int  i = 0;
-		  int f  = 0;
-		  boolean m = true;
-		  
-		 
-		  Scanner sn = new Scanner(System.in);
-		 
-		  do {
-		   System.out.print("input User Name"+" ");
-		   String n = sn.nextLine();
-		   
-		   System.out.print("input Password"+" ");
-		   String p = sn.nextLine();
-		   
-		    
-		   
-		    if(n.equals(name)&&(p.equals(pass))){
-		     i=3;
-		   do {
-		            System.out.print("User :: "+name);
-		            System.out.println(" \tยอดเงินคงเหลือ  = "+ b);
-		            
-		            System.out.print("ระบุจำนวนเงินที่ต้องการถอน "+" ");
-		            f = sn.nextInt(); 
-		           
-		   if(f>b) {
-		    System.out.println(" เงินในบัญชีมีไม่พอจ่าย.");
-		   } else if(f<100) {
-		    System.out.println("ถอนขั้นต่ำ 100 บาท ");
-		   } else if(f%100 !=0) {
-		    System.out.println(" ตู้สามารถจ่ายแบงค์ 100 500 1000 ได้เท่านั้น");
-		   }else {
-		    b = b-f;
-		    System.out.println("คุณถอนไปจำนวน  "+ f + "  บาท "+ "\tจำนวนเงินคงเหลือ  "+b);
-		    m=false;
-		   }
-		   
-		   }while(m);
 
-		       } else {
-		       i++;
-		       System.out.println("ขออภัยคุณใส่รหัสผิด  "+i+" ครั้ง");
-		       }
-		  }while(i<3);
-		  System.out.println("จบการทำงาน");
-		  }
+import java.util.Scanner;
+
+public class Nareerat {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "nareerat";
+		String pass = "phukan";
+		double b = 5000;
+		int i = 0;
+		int f = 0;
+		boolean m = true;
+
+		Scanner sn = new Scanner(System.in);
+
+		do {
+			System.out.print("input User Name" + " ");
+			String n = sn.nextLine();
+
+			System.out.print("input Password" + " ");
+			String p = sn.nextLine();
+
+			if (n.equals(name) && (p.equals(pass))) {
+				// i = 3;
+				do {
+					System.out.print("User :: " + name);
+					System.out.println(" \tยอดเงินคงเหลือ  = " + b);
+
+					System.out.print("ระบุจำนวนเงินที่ต้องการถอน " + " ");
+					f = sn.nextInt();
+
+					if (f > b) {
+						System.out.println(" เงินในบัญชีมีไม่พอจ่าย.");
+					} else if (f < 100) {
+						System.out.println("ถอนขั้นต่ำ 100 บาท ");
+					} else if (f % 100 != 0) {
+						System.out.println(" ตู้สามารถจ่ายแบงค์ 100 500 1000 ได้เท่านั้น");
+					} else {
+						b = b - f;
+						System.out.println("คุณถอนไปจำนวน  " + f + "  บาท " + "\tจำนวนเงินคงเหลือ  " + b);
+						m = false;
+					}
+
+				} while (m);
+				System.out.println("จบการทำงาน");
+				return;
+			} else {
+				i++;
+				System.out.println("ขออภัยคุณใส่รหัสผิด  " + i + " ครั้ง");
+			}
+		} while (i < 3);
+		// System.out.println("จบการทำงาน");
 	}
+}
+//การตั้งชื่อตัวแปรมีผล
