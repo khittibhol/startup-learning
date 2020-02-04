291b8822933b9e83183f4b66b664f81947c91bbd
khittibhol <khittibhol@gmail.com>
2020-02-05 02:35:37 +0700

    Rewrite example when keeping the records by collection and multiple elements

--- workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
+++ workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
@@ -3,6 +3,8 @@ package ac.th.ksu.dit;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author khittibhol
@@ -15,17 +17,32 @@ public class CalculateExpensiveMobileNumber {
 		FileReader file = new FileReader("resources/number.txt");
 		BufferedReader reader = new BufferedReader(file);
 		try {
+			List<Object[][]> groupMobile = new ArrayList<>();
 			// To do write code here.
-//			int i = 1;
+//			int i = 0;
 			String data;
 			System.out.println("========= Start for Calculate the expensive by mobile no. ========");
 			while ((data = reader.readLine()) != null) {
 //				System.out.println(i + " : " + data);
-//				i = i + 1;
+				
 				String readData[] = data.split(",");
-				System.out.println("Mobile No.\t: "+readData[0]+"\tTiming : "+readData[1]+"\nExpense : "+readData[2]);
-				System.out.println("=======================================================");
+//				System.out.println("Mobile No.\t: "+readData[0]+"\tTiming : "+readData[1]+"\nExpense : "+readData[2]);
+//				System.out.println("=======================================================");
+				
+				String[] mobile = new String[] {readData[0] };
+				String[] timingAndExpense = new String[] { readData[1], readData[2]};
+				
+				groupMobile.add(new Object[][]{ mobile, timingAndExpense });
+				
 			}
+			
+			for (int i = 0; i < groupMobile.size(); i++) {
+				Object[][] element = groupMobile.get(i);
+				String[] mobile = (String[]) element[0];
+				String[] timinAndExpense = (String[]) element[1];
+				System.out.println(mobile[0]  + " : "+timinAndExpense[0] + " : "+ timinAndExpense[1]);
+			}
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
