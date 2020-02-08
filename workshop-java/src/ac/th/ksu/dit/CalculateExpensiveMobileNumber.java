385128c6c00ef166cc15d7b7f02c039232b31564
N@patwarun <napatwarun.sr@ksu.ac.th>
2020-02-09 02:22:35 +0700

    Update Cal 9-2-20

--- workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
+++ workshop-java/src/ac/th/ksu/dit/CalculateExpensiveMobileNumber.java
@@ -1,9 +1,12 @@
 package ac.th.ksu.dit;
 
 import java.io.BufferedReader;
+import java.util.*;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 
 /**
@@ -17,56 +20,31 @@ public class CalculateExpensiveMobileNumber {
 		FileReader file = new FileReader("resources/number.txt");
 		BufferedReader reader = new BufferedReader(file);
 		try {
-			// List<Object[][]> groupMobile = new ArrayList<>();
-			// To do write code here.
-			int i = 0;
-			String dataMobile[][]=new String[150][3];
 			String data;
+			List<String[]> dataExpense = new ArrayList<>();
+			Hashtable<String, String[]> table = new Hashtable<>();
 			System.out.println("========= Start for Calculate the expensive by mobile no. ========");
 			while ((data = reader.readLine()) != null) {
-//				System.out.println(i + " : " + data);
-
 				String readData[] = data.split(",");
-			//	System.out.println("Mobile No.\t: "+readData[0]+"\tTiming : "+readData[1]+"\tExpense : "+readData[2]);
-			//	System.out.println("=======================================================");
-				//
-				// String[] mobile = new String[] { readData[0] };
-				// String[] timingAndExpense = new String[] { readData[1],
-				// readData[2] };
-				//
-				// groupMobile.add(new Object[][] { mobile, timingAndExpense });
-				
-				dataMobile[i][0] = readData[0];
-				dataMobile[i][1] = readData[1];
-				dataMobile[i][2] = readData[2];
-				//-------------
-				/*
-				for (int r = 0; r < dataMobile.length; r++) {
-					for (int c = 0; c < dataMobile[r].length; c++) {
-						dataMobile[r][c] = readData[c];
-						System.out.println(dataMobile.length);
-						System.out.println(dataMobile[r].length);
-						
-					}
-				}
-				*/
-				//----------
-				i++;
-			}
-			for (String[] a : dataMobile) {
-				for (String b : a) {
-					System.out.print(b + "  ");
-				}
-				System.out.println();
-			}
-			// for (int i = 0; i < groupMobile.size(); i++) {
-			// Object[][] element = groupMobile.get(i);
-			// String[] mobile = (String[]) element[0];
-			// String[] timinAndExpense = (String[]) element[1];
-			// System.out.println(i+" : "+mobile[0] + " : " + timinAndExpense[0]
-			// + " : " + timinAndExpense[1]);
-			// }
+				dataExpense.add(data.split(","));
+				if (table.containsKey(readData[0])) {
+					String[] getData = table.get(readData[0]);
+					BigDecimal dTiming = new BigDecimal(getData[0]);
+					BigDecimal timing = dTiming.add(new BigDecimal(readData[0]));
+					BigDecimal dExpense = new BigDecimal(getData[1]);
+					BigDecimal expense = dExpense.add(new BigDecimal(readData[1]));
 
+					table.put(readData[0], new String[] { timing.toPlainString(), expense.toPlainString() });
+				} else {
+					table.put(readData[0], new String[] { readData[1], readData[2] });
+				}
+
+				table.forEach( 
+			            (key,value) -> System.out.println("Telephone Number : " + key + ", \tTiming : " + value[0]+ ", \t\tExpense : " + value[1])); 			
+				}
+
+			
+			System.out.println("HashTable sizing : " + table.size());
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
